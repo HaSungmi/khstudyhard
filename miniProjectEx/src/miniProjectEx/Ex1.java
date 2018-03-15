cebfb36183f5145531cdecb9c87d73ca68b1b561
gk___ <gk___@LAPTOP-FA2N9IT0>
2018-03-16 03:07:13 +0900

    //이벤트 8번 이상하면 오류, 라벨엔 이벤트 적용안됨

---
+++ miniProjectEx/src/miniProjectEx/Ex1.java
@@ -0,0 +1,127 @@
+package miniProjectEx;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ex1 extends JFrame{
+	
+	public Ex1(){
+		setLocation(200,100);
+		setBounds(100,100,800,600); //(x, y, width, height)
+		//사이즈 조정 못하게
+		//setResizable(false);
+
+		JPanel base = new JPanel();
+		JPanel jp1 = new JPanel();
+		JPanel jp2 = new JPanel();
+		
+		base.setLayout(new GridLayout(2,1,1,1));
+		
+		JPanel timejp = new JPanel();
+		timejp.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		timejp.add(new JLabel("시간 나오는 라벨"));
+		
+		jp1.setLayout(new BorderLayout());
+		jp1.add(new JLabel(new ImageIcon("images_1.jpg")), BorderLayout.CENTER);
+		
+		jp1.add(timejp, BorderLayout.NORTH);
+		jp2.setLayout(new BorderLayout());
+		JPanel jp2_1 = new JPanel();
+		jp2_1.setLayout(new GridLayout(1,5,1,1));
+		jp2.add(jp2_1, BorderLayout.CENTER);
+		jp2.add(new JLabel(" "),BorderLayout.NORTH);
+		jp2.add(new JLabel(" "),BorderLayout.EAST);
+		jp2.add(new JLabel(" "),BorderLayout.WEST);
+		jp2.add(new JLabel(" "),BorderLayout.SOUTH);
+		
+		
+		JPanel stu1 = new JPanel();
+		JPanel stu2 = new JPanel();
+		JPanel stu3 = new JPanel();
+		JPanel stu4 = new JPanel();
+		JPanel stu5 = new JPanel();
+		
+		JPanel stu1_l = new JPanel();
+		JPanel stu2_l = new JPanel();
+		JPanel stu3_l = new JPanel();
+		JPanel stu4_l = new JPanel();
+		JPanel stu5_l = new JPanel();
+		
+		JPanel stu1_P = new JPanel();
+		JPanel stu2_P = new JPanel();
+		JPanel stu3_P= new JPanel();
+		JPanel stu4_P = new JPanel();
+		JPanel stu5_P = new JPanel();
+		
+		stu1_l.add(new JLabel("학생 1"));
+		stu2_l.add(new JLabel("학생 2"));
+		stu3_l.add(new JLabel("학생 3"));
+		stu4_l.add(new JLabel("학생 4"));
+		stu5_l.add(new JLabel("학생 5"));
+		
+		JProgressBar pbar1_1, pbar1_2, pbar1_3, pbar2_1, pbar2_2, pbar2_3, pbar3_1, pbar3_2, pbar3_3;
+		JProgressBar pbar4_1, pbar4_2, pbar4_3, pbar5_1, pbar5_2, pbar5_3; 
+		
+		stu1_P.setLayout(new GridLayout(3,1,1,5));
+		stu1_P.add(pbar1_1 = new JProgressBar());
+		pbar1_1.setStringPainted(true);
+		stu1_P.add(pbar1_2 = new JProgressBar());
+		stu1_P.add(pbar1_3 = new JProgressBar());
+		
+		stu2_P.setLayout(new GridLayout(3,1,1,5));
+		stu2_P.add(pbar2_1 = new JProgressBar());
+		stu2_P.add(pbar2_2 = new JProgressBar());
+		stu2_P.add(pbar2_3 = new JProgressBar());
+		
+		stu3_P.setLayout(new GridLayout(3,1,1,5));
+		stu3_P.add(pbar3_1 = new JProgressBar());
+		stu3_P.add(pbar3_2 = new JProgressBar());
+		stu3_P.add(pbar3_3 = new JProgressBar());
+		
+		stu4_P.setLayout(new GridLayout(3,1,1,5));
+		stu4_P.add(pbar4_1 = new JProgressBar());
+		stu4_P.add(pbar4_2 = new JProgressBar());
+		stu4_P.add(pbar4_3 = new JProgressBar());
+		
+		stu5_P.setLayout(new GridLayout(3,1,1,5));
+		stu5_P.add(pbar5_1 = new JProgressBar());
+		stu5_P.add(pbar5_2 = new JProgressBar());
+		stu5_P.add(pbar5_3 = new JProgressBar());
+
+		stu1.setLayout(new GridLayout(2,1,0,0));
+		stu2.setLayout(new GridLayout(2,1,0,0));
+		stu3.setLayout(new GridLayout(2,1,0,0));
+		stu4.setLayout(new GridLayout(2,1,0,0));
+		stu5.setLayout(new GridLayout(2,1,0,0));
+
+		stu1.add(stu1_l);
+		stu1.add(stu1_P);
+		stu2.add(stu2_l);
+		stu2.add(stu2_P);
+		stu3.add(stu3_l);
+		stu3.add(stu3_P);
+		stu4.add(stu4_l);
+		stu4.add(stu4_P);
+		stu5.add(stu5_l);
+		stu5.add(stu5_P);
+		
+		jp2_1.add(stu1);
+		jp2_1.add(stu2);
+		jp2_1.add(stu3);
+		jp2_1.add(stu4);
+		jp2_1.add(stu5);
+		
+		base.add(jp1);
+		base.add(jp2);
+		
+		add(base);
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new Ex1();
+	}
+
+}
---
+++ miniProjectEx/src/miniProjectEx/KeyEvt1.java
@@ -0,0 +1,86 @@
+package miniProjectEx;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.KeyListener;                                                                                               
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.event.KeyEvent;
+ 
+class MyFrame implements KeyListener {
+    JFrame frm = new JFrame();
+    JPanel jp1 = new JPanel();
+    JPanel jp2 = new JPanel();
+    JPanel jp3 = new JPanel();
+    JPanel jp4 = new JPanel();
+    JTextField tfield1 = new JTextField("키이벤트 예제");
+    JTextField tfield2 = new JTextField("키이벤트 예제");
+    JTextField tfield3 = new JTextField("키이벤트 예제");
+    JTextField tfield4 = new JTextField("키이벤트 예제");
+    public MyFrame()
+    {
+        frm.setBounds(120, 120, 400, 400);
+        frm.setTitle("포커스 테스트");
+        frm.setLayout(new GridLayout(2,2,2,2));
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //텍스트필드를 셋팅
+        tfield1.addKeyListener(this);
+        tfield2.addKeyListener(this);
+        tfield3.addKeyListener(this);
+        tfield4.addKeyListener(this);
+        jp1.add(tfield1);
+        jp2.add(tfield2);
+        jp3.add(tfield3);
+        jp4.add(tfield4);
+        frm.add(jp1);
+        frm.add(jp2);
+        frm.add(jp3);
+        frm.add(jp4);
+        frm.setVisible(true);
+    }
+    @Override
+    public void keyPressed(KeyEvent e) 
+    {
+        ShowInfo(e, "키누름");
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+        	tfield1.setText(e.getKeyText(e.getKeyCode()));
+        	jp1.setBackground(Color.BLACK);
+        }else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+        	tfield2.setText(e.getKeyText(e.getKeyCode()));
+        }else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+        	tfield3.setText(e.getKeyText(e.getKeyCode()));
+        }else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+        	tfield4.setText(e.getKeyText(e.getKeyCode()));
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent arg0) 
+    {
+        ShowInfo(arg0, "키풀림");        
+    }
+    @Override
+    public void keyTyped(KeyEvent arg0) 
+    {
+        ShowInfo(arg0, "타이핑");
+    }
+    
+    public void ShowInfo(KeyEvent e, String s)
+    {
+        char c = e.getKeyChar();
+        int keyCode = e.getKeyCode();
+        String modifiers = e.isAltDown() + " " + e.isControlDown() + " " + e.isShiftDown() ;
+        System.out.println( s + " : " + c + " : " + keyCode + " : " + modifiers);
+//        tfield1.setText(e.getKeyText(e.getKeyCode()));
+//        tfield2.setText(e.getKeyText(e.getKeyCode()));
+//        tfield3.setText(e.getKeyText(e.getKeyCode()));
+//        tfield4.setText(e.getKeyText(e.getKeyCode()));
+    }
+}
+ 
+public class KeyEvt1 {
+    public static void main(String[] args){
+        MyFrame my = new MyFrame();
+    }
+}
