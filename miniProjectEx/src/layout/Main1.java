dfaa09d19b18f48b50286e98ff964b9088aa096e
gk___ <gk___@LAPTOP-FA2N9IT0>
2018-03-19 01:53:46 +0900

    //

--- miniProjectEx/src/layout/Main1.java
+++ miniProjectEx/src/layout/Main1.java
@@ -3,29 +3,73 @@ package layout;
 import java.awt.CardLayout;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Main1 {
+public class Main1 extends JFrame implements MouseListener{
+	CardLayout card;
+	 JPanel jp1;
+	 JPanel jp2;
+	 JPanel jp3;
 
 	public static void main(String[] args) {
+		new Main1();
+	}
+	
+	public Main1(){
 		JFrame jf = new JFrame();
-		JPanel jp1, jp2, jp3;
 		
+
 		jf.setBounds(100,100,500,300);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setLayout(new GridLayout(1,3,5,5));
+		jf.setLayout(card = new CardLayout());
 		
-		jf.add(jp1 = new Panel1());
-		jf.add(jp2 = new Panel2());
-		jf.add(jp3 = new Panel3());
+		jf.add(jp1 = new Panel1(),"1");
+		jf.add(jp2 = new Panel2(),"2");
+		jf.add(jp3 = new Panel3(),"3");
+		jp1.addMouseListener(this);
+		jp2.addMouseListener(this);
+		jp3.addMouseListener(this);
 		
 		jf.setVisible(true);
 		
 		//vo -> panel여러개
-		//manager -> 각 패널 프레임에 붙이고 관리(이벤트)
+		//manager -> 각 패널들 프레임에 붙이고 관리(이벤트)
 		//main -> 실행클래스
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
