28cf7f21906f2bed88725b68d0a070d73360dee3
gk___ <gk___@LAPTOP-FA2N9IT0>
2018-03-20 22:02:01 +0900

    //joption뜰 때 타이머 멈추기 완료

--- miniProjectEx/src/miniProjectEx/Ex3.java
+++ miniProjectEx/src/miniProjectEx/Ex3.java
@@ -33,6 +33,7 @@ public class Ex3 extends JFrame implements KeyListener {
 	JProgressBar timeP;
 	Image dieImage;
 	JLabel[] lifeLabel = new JLabel[3];
+	Timer timer = new Timer();
 
 	// 실행용 메인메소드
 	public static void main(String[] args) {
@@ -49,37 +50,26 @@ public class Ex3 extends JFrame implements KeyListener {
 		bar = new JPanel(new GridLayout(1,2,1,1));
 		barbase = new  JPanel(new FlowLayout(FlowLayout.RIGHT));
 
-//		barbase.add(timeLabel = new JLabel("남은 시간 : "+time+" | "));
 		bar.add(timeP = new JProgressBar());
 		timeP.setMinimum(0);
 		timeP.setMaximum(20);
 		timeP.setValue(time);
 		timeP.setForeground(Color.DARK_GRAY);
 		timeP.setBorderPainted(false);
-//		barbase.add(new JLabel("|"));
 		bar.add(lifePanel = new JPanel());
 		barbase.add(bar);
 		
 		lifePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-		
-//		lifePanel.add(lifeLabel1 = new JLabel());
-//		lifePanel.add(lifeLabel2 = new JLabel());
-//		lifePanel.add(lifeLabel3 = new JLabel());
+
 		
 		Image lifeImage = new ImageIcon("lifeImage.PNG").getImage().getScaledInstance(20, 20, 0);
 		dieImage = new ImageIcon("baseImg.PNG").getImage().getScaledInstance(20, 20, 0);
 
-//		lifeLabel1.setIcon(new ImageIcon(lifeImage));
-//		lifeLabel2.setIcon(new ImageIcon(lifeImage));
-//		lifeLabel3.setIcon(new ImageIcon(lifeImage));
-		
 		
 		for(int i = 0; i<life; i++){
 			lifePanel.add(lifeLabel[i] = new JLabel());
 			lifeLabel[i].setIcon(new ImageIcon(lifeImage));
 		}
-
-//		lifePanel.add(lifeLabel1 = new JLabel("남은 라이프 : " + life + "개"));
 
 		jp = new JPanel();
 		jp.setLayout(new GridLayout(3, 5, 10, 10));
@@ -108,7 +98,7 @@ public class Ex3 extends JFrame implements KeyListener {
 			l_name[i].requestFocus();
 		}
 		if(time==20){
-			new Timer().start();
+				timer.start();
 		}
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -130,8 +120,9 @@ public class Ex3 extends JFrame implements KeyListener {
 
 				if (count >= 15) {
 					JOptionPane.showMessageDialog(null, "남은 횟수는 " + (3 - count2) + "회 입니다.");
-
-					
+					synchronized(timer){
+						timer.notify();
+					}
 					count = 0;
 					count2++;
 					if (count2 >= 4) {			//마지막 run을 실행 시키지 않음
@@ -144,7 +135,7 @@ public class Ex3 extends JFrame implements KeyListener {
 				}
 			} else { // 틀릴경우
 				life--;
-//				lifeLabel.setText("남은 라이프 : " + life + "개");
+
 				if(life==2){
 					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 				}else if(life==1){
@@ -152,7 +143,7 @@ public class Ex3 extends JFrame implements KeyListener {
 				}
 				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]"); // title은 자동으로 바뀌지 않아서 다시 setTitle
 				if (life == 0) { // 라이프가 0일 경우
-					lifeLabel1.setIcon(new ImageIcon(dieImage));
+					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 					JOptionPane.showMessageDialog(null, "라이프를 모두 소진하셨습니다.");
 					System.exit(0); // 시스템 종료 -> ***이 부분은 상위 뷰와 연결될 때 수정해야함***
 				}
@@ -167,9 +158,9 @@ public class Ex3 extends JFrame implements KeyListener {
 
 				if (count >= 15) {
 					JOptionPane.showMessageDialog(null, "남은 횟수는 " + (3 - count2) + "회 입니다.");
-					Timer timer = new Timer();
-
-					
+					synchronized(timer){
+						timer.notify();
+					}
 					count = 0;
 					count2++;
 					if (count2 >= 4) {
@@ -182,7 +173,7 @@ public class Ex3 extends JFrame implements KeyListener {
 				}
 			} else {
 				life--;
-//				lifeLabel.setText("남은 라이프 : " + life + "개");
+
 				if(life==2){
 					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 				}else if(life==1){
@@ -191,7 +182,7 @@ public class Ex3 extends JFrame implements KeyListener {
 				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
 
 				if (life == 0) {
-					lifeLabel1.setIcon(new ImageIcon(dieImage));
+					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 					JOptionPane.showMessageDialog(null, "라이프를 모두 소진하셨습니다.");
 					System.exit(0);
 				}
@@ -202,12 +193,12 @@ public class Ex3 extends JFrame implements KeyListener {
 			if (solNum == '2') {
 				l_name[count].setIcon(new ImageIcon("Capybara.jpg"));
 				count++;
-
+				
 				if (count >= 15) {
 					JOptionPane.showMessageDialog(null, "남은 횟수는 " + (3 - count2) + "회 입니다.");
-					Timer timer = new Timer();
-
-					
+					synchronized(timer){
+						timer.notify();
+					}
 					count = 0;
 					count2++;
 					if (count2 >= 4) {
@@ -219,7 +210,7 @@ public class Ex3 extends JFrame implements KeyListener {
 				}
 			} else {
 				life--;
-//				lifeLabel.setText("남은 라이프 : " + life + "개");
+
 				if(life==2){
 					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 				}else if(life==1){
@@ -227,7 +218,7 @@ public class Ex3 extends JFrame implements KeyListener {
 				}
 				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
 				if (life == 0) {
-					lifeLabel1.setIcon(new ImageIcon(dieImage));
+					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 					JOptionPane.showMessageDialog(null, "라이프를 모두 소진하셨습니다.");
 					System.exit(0);
 				}
@@ -241,10 +232,10 @@ public class Ex3 extends JFrame implements KeyListener {
 				count++;
 
 				if (count >= 15) {
-					JOptionPane.showMessageDialog(null, "남은 횟수는 " + (3 - count2) + "회 입니다.");
-					Timer timer = new Timer();
-
-					
+					JOptionPane.showMessageDialog(null, "남은 횟수는 " + (3 - count2) + "회 입니다.");	
+					synchronized(timer){
+						timer.notify();
+					}
 					count = 0;
 					count2++;
 					if (count2 >= 4) {
@@ -257,7 +248,6 @@ public class Ex3 extends JFrame implements KeyListener {
 				}
 			} else {
 				life--;
-//				lifeLabel.setText("남은 라이프 : " + life + "개");
 				if(life==2){
 					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 				}else if(life==1){
@@ -265,7 +255,7 @@ public class Ex3 extends JFrame implements KeyListener {
 				}
 				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
 				if (life == 0) {
-					lifeLabel1.setIcon(new ImageIcon(dieImage));
+					lifeLabel[life].setIcon(new ImageIcon(dieImage));
 					JOptionPane.showMessageDialog(null, "라이프를 모두 소진하셨습니다.");
 					System.exit(0);
 				}
@@ -297,21 +287,24 @@ public class Ex3 extends JFrame implements KeyListener {
 			time = 20;
 			while(true){
 				try {
+					if (count == 15) {
+						synchronized(timer){
+							timer.wait();
+						}
+					}
+						Thread.sleep(1000);
 
-					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				time--;
 				if(time==0){
-//					timeLabel.setText("남은 시간 : "+time+" | ");
 					timeP.setValue(time);
 					JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
 					System.exit(0);
 					break;
 				}
-//				timeLabel.setText("남은 시간 : "+time+" | ");
 				timeP.setValue(time);
 			}
 
